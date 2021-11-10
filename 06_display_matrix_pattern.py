06e19e19952551d22f3f69a6c9c5bc5459cab6e3
patrickryan <theyoungsoul@gmail.com>
2021-11-09 21:58:48 -0600

    adding some comments

--- 06_display_matrix_pattern.py
+++ 06_display_matrix_pattern.py
@@ -11,18 +11,24 @@ if __name__ == '__main__':
     battery_level = db_tello.get_battery()
     print(f"Battery Life Percentage: {battery_level}")
 
-    db_tello.display_heart()
-    time.sleep(3)
+    # Use the Tello SDK 3.0 API to display pattern
+    # display a blue 'up' arrow
+    db_tello.send_command_with_return("EXT mled g 000bb00000bbbb000bbbbbb0000bb000000bb000000bb000000bb000000bb000")
+    time.sleep(5)
 
+    # display blue 'up' arrow using the DroneBlocks Tello method
     db_tello.display_up_arrow()
     time.sleep(3)
 
+    # change the color of the arrow from blue to purple
     db_tello.display_up_arrow(display_color=DroneBlocksTello.PURPLE)
     time.sleep(3)
 
+    # change the color of the arrow to red
     db_tello.display_up_arrow(display_color=DroneBlocksTello.RED)
     time.sleep(3)
 
+    # display a down arrow
     db_tello.display_down_arrow()
     time.sleep(3)
 
@@ -49,6 +55,10 @@ if __name__ == '__main__':
 
     db_tello.display_right_arrow(display_color=DroneBlocksTello.RED)
     time.sleep(3)
+
+    db_tello.display_heart()
+    time.sleep(3)
+
 
     db_tello.clear_everything()
 
