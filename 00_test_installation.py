c60af11b5c215feba97b94239a86bf37aef5f584
patrickryan <theyoungsoul@gmail.com>
2021-12-21 12:58:05 -0600

    added Usage comments

--- 00_test_installation.py
+++ 00_test_installation.py
@@ -2,6 +2,13 @@ from droneblocks.DroneBlocksTello import DroneBlocksTello
 import cv2
 import droneblocks
 
+"""
+Usage
+
+python 00_test_installation.py
+
+"""
+
 if __name__ == '__main__':
     print(f"DroneBlocks Util Version: {droneblocks.__version__}")
     db_tello = DroneBlocksTello()
